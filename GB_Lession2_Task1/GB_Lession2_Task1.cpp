b694ca0f5419fa0f04f2cefb422decef06b3e63e
Dmitriy Aristarkhov <aaarrriiisss335@gmail.com>
2022-05-14 05:06:29 +0300

    Добавьте файлы проекта.

---
+++ GB_Lession2_Task1/GB_Lession2_Task1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <stdbool.h>
+#include <stdio.h>
+
+
+bool Primer(int n)
+{
+    if (n > 1)
+    {
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    else
+        return false;  
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter some number: ");
+
+    scanf_s("%d", &n);
+
+    printf("It's prime: %d\n", Primer(n));
+}
